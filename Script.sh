42611ab27a529e0a458c023b5809e9fa691b1706
Alberto Lupatin <55557905+AlbertoLupatin@users.noreply.github.com>
2024-09-13 08:28:45 +0200

    Delete Script.sh

--- Script.sh
+++
@@ -1,5 +0,0 @@
-java -jar ../Tools/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ../Anotations/human_g1k_v37.fasta -I ../1_Preprocessing/Control.sorted.bam -o realigner_c.intervals
-java -jar ../Tools/GenomeAnalysisTK.jar -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I ../1_Preprocessing/Control.sorted.bam -targetIntervals realigner_c.intervals -o Control.sorted.realigned.bam
-
-java -jar ../Tools/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ../Anotations/human_g1k_v37.fasta -I ../1_Preprocessing/Tumor.sorted.bam -o realigner_t.intervals
-java -jar ../Tools/GenomeAnalysisTK.jar -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I ../1_Preprocessing/Tumor.sorted.bam -targetIntervals realigner_intervals.t -o Tumor.sorted.realigned.bam
